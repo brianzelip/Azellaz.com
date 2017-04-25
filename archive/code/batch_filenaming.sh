8d8ef2cf8084931253c9588a1d1f1e0f32066931
Brian Zelip <bzelip@gmail.com>
2017-04-25 19:56:51 -0400

    deploy 50x50 thumbs 👍👍👍👍👍

--- archive/code/batch_filenaming.sh
+++ archive/code/batch_filenaming.sh
@@ -4,6 +4,17 @@
 
 # prefix thumbnail filenames
 for f in * ; do mv "$f" "thumb-med-$f" ; done
+for f in * ; do mv "$f" "thumb-sm-$f" ; done
 
 # suffix thumbnail filenames
 for file in *.jpg; do mv "$file" "${file/.jpg/-thumb-med.jpg}"; done
+
+#
+# ImageMagick
+#
+
+# Medium thumbnails, 25% of OG pic (og pics come from one directory up)
+mogrify -resize 25% *.jpg
+
+# Smallest thumbnails, 50px by 50px, no matter the dimensions of the OG pic (og pics come from one directory up)
+mogrify -resize 50x50 *.jpg
