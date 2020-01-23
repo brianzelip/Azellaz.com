1e23de472dc031d620876cf834fa2c6966025f58
Brian Zelip <bzelip@gmail.com>
2020-01-23 16:48:03 -0500

    newProductPages.js: Refactor for creating json files
    The solution still is not present because the page
    file name has to be figured out - this is why
    the slug field got added back to admin/config.yml

--- newProductPages.js
+++ newProductPages.js
@@ -1,32 +1,62 @@
 const fs = require('fs');
-const products = require('./_data/products.json');
 
-const productFilesDir = './products/';
+const productDataDir = './_data/allproducts';
+const productPagesDir = './_products/';
 
-removeOldProductFiles(productFilesDir);
-createNewProductFiles(productFilesDir);
+removeOldProductPages();
+createNewProductPages();
 
-function removeOldProductFiles(dir) {
-  const oldFiles = fs.readdirSync(dir);
+function removeOldProductPages() {
+  const oldFiles = fs.readdirSync(productPagesDir);
 
   console.log(
-    `Deleting ${oldFiles.length} old product files in ${productFilesDir} ...`
+    `Deleting ${oldFiles.length} old Azellaz product files in ${productPagesDir} ...`
   );
 
-  let count = 0;
-  for (const file of oldFiles) {
-    count++;
-    console.log(`${count}. ${file}`);
-    fs.unlinkSync(`${dir}${file}`);
-  }
+  oldFiles.forEach((file, i) => {
+    console.log(`${i}. ${file}`);
+    fs.unlinkSync(`${productPagesDir}${file}`);
+  });
 
   console.log('\n');
-
   return;
 }
 
-function createNewProductFiles(dir) {
-  console.log(`Generating new Azellaz product pages in ${productFilesDir} ...`);
+function createNewProductPages() {
+  console.log(`Generating new Azellaz product pages in ${productPagesDir} ...`);
+
+  const dataFiles = fs.readdirSync(productDataDir);
+
+  dataFiles.forEach((path, i) => {
+    const data = JSON.parse(fs.readFileSync(`${productDataDir}${path}`));
+
+    if (data.currentListing) {
+      const pageContent = `---
+layout: product
+name: "${data.name}"
+displayName: "${data.displayName}"
+id: "${data.id}"
+currentListing: ${data.currentListing}
+inStock: ${data.inStock}
+stockOnHand: ${data.stockOnHand}
+type: "${data.type}"
+descriptionShort: "${data.descriptionShort}"
+descriptionLong: ${data.descriptionLong}
+materials: ${data.materials}
+price: ${data.price}
+weight: ${data.weight}
+height: ${data.height}
+width: ${data.width}
+length: ${data.length}
+imgPrimary: "${data.imgPrimary}"
+imgSecondarySet: ${data.imgSecondarySet}
+---
+`;
+
+      fs.writeFileSync(`${dir}${product.slug}.html`, fileContent);
+      console.log(`${count}. ${product.slug}.html`);
+    }
+  });
 
   let count = 0;
 
