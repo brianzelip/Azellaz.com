0c91b26b0c6d9824e58f4befac05b8181a1d2195
Brian Zelip <bzelip@gmail.com>
2020-02-03 16:09:35 -0500

    productsDataToProductsFiles.js: Refactor to generate md files

--- archive/code/productsDataToProductsFiles.js
+++ archive/code/productsDataToProductsFiles.js
@@ -1,34 +1,45 @@
 const fs = require('fs');
 const data = require('./_data/products.json');
 
-data.forEach((obj, i) => {
-  const content = {
-    layout: 'product',
-    name: obj.name,
-    displayName: obj.displayName,
-    id: obj.id,
-    slug: obj.slug,
-    currentListing: obj.currentListing,
-    inStock: obj.inStock,
-    stockOnHand: obj.stockOnHand,
-    type: obj.type,
-    descriptionShort: obj.descriptionShort,
-    descriptionLong: obj.descriptionLong,
-    materials: obj.materials,
-    price: obj.price,
-    weight: obj.weight,
-    height: obj.height,
-    width: obj.width,
-    length: obj.length,
-    imgPreAzellazV2: true,
-    imgPrimary: obj.imgPrimary.split('.')[0], //split for Cloudinary
-    imgSecondarySet: obj.imgSecondarySet.map(img => img.split('.')[0])
-  };
+function arrJS2Yaml(arr) {
+  return arr.length > 0
+    ? arr
+        .map(
+          item => `
+  - "${item}"`
+        )
+        .join('')
+    : ``;
+}
 
-  fs.writeFileSync(
-    `./_data/products/${obj.slug}.json`,
-    JSON.stringify(content, null, 2)
-  );
-  console.log(`FILE #${i} WRITTEN: ${obj.slug}.json 🎉`);
+data.forEach((obj, i) => {
+  const content = `---
+layout: product
+name: ${obj.name}
+displayName: ${obj.displayName}
+id: ${obj.id}
+slug: ${obj.slug}
+currentListing: ${obj.currentListing}
+inStock: ${obj.inStock}
+stockOnHand: ${obj.stockOnHand}
+type: ${obj.type}
+descriptionShort: ${obj.descriptionShort}
+materials: ${arrJS2Yaml(obj.materials)}
+price: ${obj.price}
+weight: ${obj.weight}
+height: ${obj.height}
+width: ${obj.width}
+length: ${obj.length}
+imgPrimary: ${obj.imgPrimary.split('.')[0]}
+imgSecondarySet: ${arrJS2Yaml(
+    obj.imgSecondarySet.map(img => img.split('.')[0])
+  )}
+---
+
+${obj.descriptionLong.join('\n\n')}
+`;
+
+  fs.writeFileSync(`./_data/products/${obj.slug}.md`, content);
+  console.log(`FILE #${i} WRITTEN: ${obj.slug}.md 🎉`);
   console.log(content);
 });
