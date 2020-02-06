dd5504ca1f51a1e4a42c3aef09e3ce6c0138578b
Brian Zelip <bzelip@protonmail.com>
2020-02-06 06:28:56 -0500

    newProductPages.js: Close permalink quote

--- newProductPages.js
+++ newProductPages.js
@@ -51,7 +51,7 @@ function createNewProductPages() {
       count++;
       const pageContent = `---
 layout: product
-permalink: "${data.permalink}
+permalink: "${data.permalink}"
 name: "${data.name}"
 displayName: "${data.displayName}"
 slug: "${data.slug}"
