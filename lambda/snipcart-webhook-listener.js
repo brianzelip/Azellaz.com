1e9cf505b9263c9c6a4b7f29f86f2d7cf3444876
Brian Zelip <bzelip@protonmail.com>
2020-06-23 11:15:12 -0400

    lambda: Move var scopes

--- lambda/snipcart-webhook-listener.js
+++ lambda/snipcart-webhook-listener.js
@@ -2,18 +2,18 @@ const https = require('https');
 
 console.log('Snipcart webhook received!');
 
-const method = 'POST';
-const host = 'api.netlify.com';
-const path =
-  '/build_hooks/5ef219aec0c4ea9331e5fe67?trigger_branch=master&trigger_title=Triggered+by+Netlify+Functions+via+Snipcart+webhook';
-const options = {
-  method,
-  host,
-  path
-};
-
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const body = event.body;
+
+  const method = 'POST';
+  const host = 'api.netlify.com';
+  const path =
+    '/build_hooks/5ef219aec0c4ea9331e5fe67?trigger_branch=master&trigger_title=Triggered+by+Netlify+Functions+via+Snipcart+webhook';
+  const options = {
+    method,
+    host,
+    path
+  };
 
   if (body.eventName && body.eventName === 'order.completed') {
     console.log('ORDER UP 🎉');
