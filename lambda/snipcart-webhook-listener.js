9f336dab5a37876fdae991cb74309f31f9db068a
Brian Zelip <bzelip@protonmail.com>
2020-06-23 16:06:34 -0400

    lambda/: Update deps and fetch

--- lambda/snipcart-webhook-listener.js
+++ lambda/snipcart-webhook-listener.js
@@ -1,4 +1,4 @@
-const get = require('simple-get');
+const fetch = require('node-fetch');
 
 console.log('Snipcart webhook received!');
 
@@ -26,4 +26,38 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: 'Thanks for the webhook snipcart!'
   };
+};
+
+exports.handler = async (event) => {
+  const body = JSON.parse(event.body);
+  const netlifyBuildHook =
+    'https://api.netlify.com/build_hooks/5ef219aec0c4ea9331e5fe67?trigger_branch=master&trigger_title=Triggered+by+Netlify+Functions+via+Snipcart+webhook';
+
+  if (body.eventName && body.eventName === 'order.completed') {
+    console.log('HERE COMES THE NEW IF SUCCESS STATEMENT!!!....');
+    const response = await fetch(netlifyBuildHook, {
+      method: 'POST'
+    })
+      .then((response) => {
+        console.log('response from THEN():::::', response);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  return {
+    statusCode: 200,
+    body: 'Thanks for the webhook snipcart!'
+  };
+
+  // return fetch(netlifyBuildHook, { method: 'POST' })
+  //   .then((res) => {
+  //     console.log('hello from the _new_ THEN()!!');
+  //     console.log('typeof res::::::::', typeof res);
+  //     console.log('res is:::', res);
+  //     return {
+  //       statusCode: 200,
+  //       body: 'Thanks for the webhook snipcart!'
+  //     };
+  //   })
+  //   .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
