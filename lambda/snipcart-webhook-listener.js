020c52f60a0fc7c9f8909d4633ff781f65091d76
Brian Zelip <bzelip@protonmail.com>
2020-06-23 11:05:14 -0400

    lambda/: Send conditional POST req to netlify build hook

--- lambda/snipcart-webhook-listener.js
+++ lambda/snipcart-webhook-listener.js
@@ -1,7 +1,34 @@
-exports.handler = async (event, context) => {
-  console.log('Snipcart webhook received!');
-  console.log('event', event);
-  console.log('context', context);
+const https = require('https');
+
+console.log('Snipcart webhook received!');
+
+const method = 'POST';
+const host = 'api.netlify.com';
+const path =
+  '/build_hooks/5ef219aec0c4ea9331e5fe67?trigger_branch=master&trigger_title=Triggered+by+Netlify+Functions+via+Snipcart+webhook';
+const options = {
+  method,
+  host,
+  path
+};
+
+exports.handler = async (event) => {
+  const body = event.body;
+
+  if (body.eventName && body.eventName === 'order.completed') {
+    console.log('ORDER UP 🎉');
+
+    const post = https.request(options, (res) => {
+      console.log(`POST response status: ${res.statusCode}`);
+    });
+
+    post.on('error', (e) => {
+      console.error(`Problem with POST: ${e.message}`);
+    });
+
+    post.end();
+  }
+
   return {
     statusCode: 200,
     body: 'Thanks for the webhook snipcart!'
