e1bf9d6da34b2f883c1cb14a26dcb9e0f30e465a
Brian Zelip <bzelip@protonmail.com>
2021-10-09 10:30:35 -0400

    Add filter for portfolio image name from url

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const pfolioImgName = require('./src/utils/filters/portfolioImageName.js');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/admin');
   eleventyConfig.addPassthroughCopy({ 'src/crawlers/*.txt': '/' });
+
+  eleventyConfig.addFilter('pfolioImgName', pfolioImgName);
 
   eleventyConfig.addWatchTarget('./src/css/');
 
